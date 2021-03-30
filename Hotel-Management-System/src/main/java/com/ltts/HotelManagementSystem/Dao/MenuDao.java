f933aced0cf468f2c764d90b39f9deb1ca3fc574
Gunturuvenkatasubbarao <78855797+Gunturuvenkatasubbarao@users.noreply.github.com>
2021-03-30 09:43:08 +0530

    Update MenuDao.java

--- Hotel-Management-System/src/main/java/com/ltts/HotelManagementSystem/Dao/MenuDao.java
+++ Hotel-Management-System/src/main/java/com/ltts/HotelManagementSystem/Dao/MenuDao.java
@@ -1,39 +1,26 @@
 package com.ltts.HotelManagementSystem.Dao;
-
 import java.util.List;
-
 import javax.persistence.EntityManager;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import com.ltts.HotelManagementSystem.model.Booking;
 import com.ltts.HotelManagementSystem.model.Member;
 import com.ltts.HotelManagementSystem.model.Menu;
-
-
-
 @Repository
 public class MenuDao {
-	
 	@Autowired
 	private EntityManager em;
-	
 	@Autowired
 	private SessionFactory sf;
-	
-	
 	public boolean InsertMenu(Menu f) {
 		boolean b=false;
 		Session s=null;
 		try {
 			s=sf.openSession();
 			s.beginTransaction();
-			
 			s.save(f);
-			//System.out.println(st);
 			s.getTransaction().commit();
 			
 		}
@@ -47,27 +34,12 @@ public class MenuDao {
 		
 		return b;
 	}
-	
-	
-	/*
-	 * public List<Member> getMembers(){ List }
-	 */
-	
-	public Member getMemberByEmail(String email) {
-		Member m=new Member();
-		
-		
-		return m;
-	}
 	public List<Menu> getAllMenu(){
 		Session session=sf.openSession();
         session.beginTransaction();
         
         List<Menu> li=sf.openSession().createCriteria(Booking.class).list();
-        //List<ProductsModel> product=sessionFactory.openSession().createCriteria(ProductsModel.class).list();
-        
-        session.getTransaction().commit();
-     
+		session.getTransaction().commit();
 		return li;
 		
 	}
