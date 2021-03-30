6d6c93e9a73469e5edcf413ab6bcbeb3e8831b5e
Gunturuvenkatasubbarao <78855797+Gunturuvenkatasubbarao@users.noreply.github.com>
2021-03-30 10:30:40 +0530

    Update HotelManagementSystemApplication.java

--- Hotel-Management-System/src/main/java/com/ltts/HotelManagementSystem/HotelManagementSystemApplication.java
+++ Hotel-Management-System/src/main/java/com/ltts/HotelManagementSystem/HotelManagementSystemApplication.java
@@ -1,11 +1,8 @@
 package com.ltts.HotelManagementSystem;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class HotelManagementSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HotelManagementSystemApplication.class, args);
 	}
